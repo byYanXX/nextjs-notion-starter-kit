73694a2bc982c3380f650ba65ff434b298a9e03f
Bingyang Yan <75976791+byYanXX@users.noreply.github.com>
2022-05-13 22:02:57 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'LeetCode-Summary-9586a514c86f4a2a869f8dd12a7aecf3',
+  rootNotionPageId: 'F-ck-LeetCode-9586a514c86f4a2a869f8dd12a7aecf3',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,8 +10,8 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Powehi~',
-  domain: 'https://glow-ursinia-e99.notion.site/LeetCode-Summary-9586a514c86f4a2a869f8dd12a7aecf3',
-  author: 'Yan_BingYang',
+  domain: 'https://glow-ursinia-e99.notion.site/F-ck-LeetCode-9586a514c86f4a2a869f8dd12a7aecf3',
+  author: 'Yan BingYang',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
