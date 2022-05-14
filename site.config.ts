7838a03752010e06b59db3c2ebb5142812a894d8
Bingyang Yan <75976791+byYanXX@users.noreply.github.com>
2022-05-14 21:14:11 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -18,7 +18,7 @@ export default siteConfig({
 
   // social usernames (optional)
   twitter: '',
-  github: 'byYanXX',
+  github: '',
   linkedin: '',
 
   // default notion icon and cover images for site-wide consistency (optional)
