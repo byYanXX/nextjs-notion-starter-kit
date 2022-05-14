236912fee934e39ab1f6ae7a69d065cc75adb7cf
Bingyang Yan <75976791+byYanXX@users.noreply.github.com>
2022-05-14 20:37:56 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -31,8 +31,23 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2022 {config.author}</div>
-
+      <div className={styles.copyright}>
+        <div
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+        >
+          <a href='https://hits.seeyoufarm.com'>
+            <img
+              style={{ display: 'block', margin: '0px auto' }}
+              src='https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fnextjs-notion-starter-kit-snowy-psi.vercel.app%2Fpowehi&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=true'
+            />
+          </a>
+          <div>Copyright 2022 {config.author} </div>
+        </div>
+      </div>
+      
       <div className={styles.settings}>
         {hasMounted && (
           <a
